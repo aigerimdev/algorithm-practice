d1c77d41c63b56a626326c54005ba9e00c7063d7
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-04-14 15:08:04 -0700

    implemented two fuctions

--- rotate_list.py
+++ rotate_list.py
@@ -1,4 +1,30 @@
 # Add your clarifying questions here
+# Will the value of shift by always be positive
+# Is the list always non-empty - an empty list will return an empty list
+# will the shift always be the right? yes
+# What if the shift is larger number than our length? - SHift will be smaller
+
+# input [1, 2, 3], 2
+# output [2, 3, 1]
+
+# input ['a', 'b', 'c', 'd'], 1
+# output ['d', 'a', 'b', 'c']
+
+# input ['u', 16, 'hello', 27], 3
+# output ['16', 'hello', '27', 'u']
+
+# input [9], 5233 
+# output None, shift out of bounds
 
 def rotate_list(list, shift_by):
-    pass
+    if not list or shift_by == 0:
+        return list
+    
+    right_part = list[-shift_by:]
+    left_part = list[:-shift_by]
+    return right_part + left_part
+
+print(rotate_list([1, 2, 3], 2))
+print(rotate_list(['a', 'b', 'c', 'd'], 1))
+print(rotate_list(['u', 16, 'hello', 27], 3))
+print(rotate_list([], 3))
--- skyline.py
+++ skyline.py
@@ -1,4 +1,15 @@
 # Add your clarifying questions here
 
 def skyline(building_list):
-    pass
+    buildings = []
+    max_height = 0
+
+    for height in building_list:
+        if height > max_height:
+            buildings.append(height)
+            max_height = height
+    return buildings
+
+print(skyline([1, 3, 7, 7, 3]))
+print(skyline([-1, 3, 8, 7, 3]))
+print(skyline([]))
